df1b2651bfaf9112b86f1e1765e53cbd78b8f50d
ronghe chen <102492211+ronghechen@users.noreply.github.com>
2025-04-21 12:53:25 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>ronghe chen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="hello" />
+        <Header title="ronghe chen" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
